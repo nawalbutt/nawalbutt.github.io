5ddb8c7053d56bc8e96384597cbbdfd6c86d60d1
nawalbutt <72464334+nawalbutt@users.noreply.github.com>
2021-01-05 10:11:03 +0500

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 		 <p>
-          Test Building
+          Test
         </p>
         <a
           className="App-link"
